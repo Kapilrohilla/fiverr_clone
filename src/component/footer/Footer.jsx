5f379b511f03485b9c7834b396f573ac6f1b81bc
kapilrohill <kapilrohilla2002@gmail.com>
2023-06-09 23:58:06 +0530

    footer completed right div complete karne ka mn nhi tha

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,11 +1,104 @@
 import React from 'react'
 import './Footer.scss';
+import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <div className="footer">
-            Footer
-        </div>
-    )
+            <div className="container">
+                <div className="top">
+                    <div className="cat">
+                        <h5 className="cat-title">Category</h5>
+                        <ul>
+                            <li>Graphic and Deisgn</li>
+                            <li>Digital Marketing</li>
+                            <li>Writing and Translation</li>
+                            <li>Video and animation</li>
+                            <li>Music and audio</li>
+                            <li>Programming and tech</li>
+                            <li>Data</li>
+                            <li>Business</li>
+                            <li>Lifestyle</li>
+                            <li>End to End product</li>
+                        </ul>
+                    </div>
+                    <div className="cat">
+                        <h5 className="cat-title">About</h5>
+                        <ul>
+                            <li>Graphic and Deisgn</li>
+                            <li>Digital Marketing</li>
+                            <li>Writing and Translation</li>
+                            <li>Video and animation</li>
+                            <li>Music and audio</li>
+                            <li>Programming and tech</li>
+                            <li>Data</li>
+                            <li>Business</li>
+                            <li>Lifestyle</li>
+                            <li>End to End product</li>
+                        </ul>
+                    </div>
+                    <div className="cat">
+                        <h5 className="cat-title">Support</h5>
+                        <ul>
+                            <li>Graphic and Deisgn</li>
+                            <li>Digital Marketing</li>
+                            <li>Writing and Translation</li>
+                            <li>Video and animation</li>
+                            <li>Music and audio</li>
+                            <li>Programming and tech</li>
+                            <li>Data</li>
+                            <li>Business</li>
+                            <li>Lifestyle</li>
+                            <li>End to End product</li>
+                        </ul>
+                    </div>
+                    <div className="cat">
+                        <h5 className="cat-title">Community</h5>
+                        <ul>
+                            <li>Graphic and Deisgn</li>
+                            <li>Digital Marketing</li>
+                            <li>Writing and Translation</li>
+                            <li>Video and animation</li>
+                            <li>Music and audio</li>
+                            <li>Programming and tech</li>
+                            <li>Data</li>
+                            <li>Business</li>
+                            <li>Lifestyle</li>
+                            <li>End to End product</li>
+                        </ul>
+                    </div>
+                    <div className="cat">
+                        <h5 className="cat-title">More on Fiverr</h5>
+                        <ul>
+                            <li>Graphic and Deisgn</li>
+                            <li>Digital Marketing</li>
+                            <li>Writing and Translation</li>
+                            <li>Video and animation</li>
+                            <li>Music and audio</li>
+                            <li>Programming and tech</li>
+                            <li>Data</li>
+                            <li>Business</li>
+                            <li>Lifestyle</li>
+                            <li>End to End product</li>
+                        </ul>
+                    </div>
+                </div>
+                <hr />
+                <div className="bottom">
+                    <div className="left">
+                        <div className="logo">
+                            <Link className="link">
+                                <span className="text">fiverr</span>
+                            </Link>
+                            <div className="dot">.</div>
+                        </div>
+                        <span className="copyright">© Fiverr International Ltd. 2023</span>
+                    </div>
+                    <div className="right">
+
+                    </div>
+                </div>
+            </div>
+        </div>);
 }
 
 export default Footer;
